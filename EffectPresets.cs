313931b4c49826aef38506a8742e2c3c038e1102
GustavoRibasss <gustavohenriqueribas25@gmail.com>
2026-09-12 10:08:07 -0300

    Remove arquivos desnecessários e atualizando app

--- EffectPresets.cs
+++ EffectPresets.cs
@@ -2,8 +2,9 @@ using System.Collections.Generic;
 static class EffectPresets {
  public static IEnumerable<StudioPreset> ForMode(int mode){
   foreach(var preset in StudioPreset.All())if(preset.Mode==mode)yield return preset;
-  if(mode<1||mode>15)yield break;
+  if(mode<1)yield break;
   yield return new StudioPreset{Name="Padrão do efeito",Mode=mode,Speed=3,Options=new EffectOptions()};
+  if(mode>15)yield break;
   yield return new StudioPreset{Name="Suave e lento",Mode=mode,Speed=1,Options=new EffectOptions{Intensity=80,Saturation=80}};
   yield return new StudioPreset{Name="Vivo e rápido",Mode=mode,Speed=4,Options=new EffectOptions{Intensity=100,Saturation=100}};
   yield return new StudioPreset{Name="Branco suave",Mode=mode,Speed=2,Options=new EffectOptions{CustomPalette=true,Foreground=0xFFFFFF,Grid=0xFFFFFF,Background=0,Intensity=100}};
